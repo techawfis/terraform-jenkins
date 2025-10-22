cecaf3e39a51e51a419ccba02a71cb4c36297c66
Sulaiman S <sulai135@gmail.com>
2025-10-22 22:01:46 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,91 @@
+variable "awsprops" {
+    type = map
+    default = {
+    region = "ap-south-1"
+    vpc = "vpc-001b47a0834dc0c4a"
+    ami = "ami-06fa3f12191aa3337"
+    itype = "t2.micro"
+    subnet = "subnet-0dedcea0b59bdb851"
+    publicip = true
+    keyname = "ap-mumbai-2025"
+    secgroupname = "sg-01e252ffcf3c93e95"
+  }
+  }
+
+
+provider "aws" {
+  region = lookup(var.awsprops, "region")
+}
+
+resource "aws_security_group" "terraformsgrp" {
+  name = lookup(var.awsprops, "secgroupname")
+  description = lookup(var.awsprops, "secgroupname")
+  vpc_id = lookup(var.awsprops, "vpc")
+
+  // To Allow SSH Transport
+  ingress {
+     from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  // To Allow Port 80 Transport
+  ingress {
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  // To Allow Port 8080 Transport
+  ingress {
+    from_port = 8080
+    protocol = "tcp"
+    to_port = 8080
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+
+resource "aws_instance" "project-iac" {
+  ami = lookup(var.awsprops, "ami")
+  instance_type = lookup(var.awsprops, "itype")
+  subnet_id = lookup(var.awsprops, "subnet") #FFXsubnet2
+  associate_public_ip_address = lookup(var.awsprops, "publicip")
+  key_name = lookup(var.awsprops, "keyname")
+
+
+  vpc_security_group_ids = [
+    aws_security_group.terraformsgrp.id
+  ]
+  root_block_device {
+    delete_on_termination = true
+    #iops = 150
+    volume_size = 8
+    volume_type = "gp2"
+  }
+  tags = {
+    Name ="Tomcat-Server"
+    Environment = "DEV"
+    OS = "Amazon Linux 2"
+    Managed = "IAC"
+  }
+  user_data = file("script.sh")
+  depends_on = [ aws_security_group.terraformsgrp ]
+}
+
+output "ec2instance" {
+  value = aws_instance.project-iac.public_ip
+}

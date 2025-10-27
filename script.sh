b41592a1adbd80707ab2e18fa86639b24e304ff1
techawfis <110276659+techawfis@users.noreply.github.com>
2025-10-27 23:23:31 +0530

    updated bash script

--- script.sh
+++ script.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 sudo yum update –y
 # sudo amazon-linux-extras install java-openjdk11 -y
+sudo dnf install java-17-amazon-corretto -y
+java -version
+readlink -f $(which java)
+echo 'export JAVA_HOME=/usr/lib/jvm/java-17-amazon-corretto' >> ~/.bashrc
+echo 'export PATH=$JAVA_HOME/bin:$PATH' >> ~/.bashrc
+source ~/.bashrc
+echo $JAVA_HOME
 cd /opt/
 sudo wget https://dlcdn.apache.org/tomcat/tomcat-11/v11.0.12/bin/apache-tomcat-11.0.12.tar.gz
 ls
